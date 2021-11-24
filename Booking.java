9ee16c75ab4c55312b61c30fd55ecf4f4e18857d
Sanchit-404 <74064295+Sanchit-404@users.noreply.github.com>
2021-11-24 16:04:17 +0530

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -91,7 +91,7 @@ class Cab_manager {
     public static Cab[] cabs;
     public static float fare() {
         time = 
-        // fare = distance * (total - free)cabs * """call class time_fare"""
+        // fare = distance * (total - free)cabs * ""call class time_fare""
         return 0;
     }
 }
